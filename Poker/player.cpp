f331655f7b6856a6812af161bfe2963749de184c
tisi <kris.vasilev13@gmail.com>
2025-07-22 16:21:21 +0300

    some work on in game actions

--- Poker/player.cpp
+++ Poker/player.cpp
@@ -4,32 +4,32 @@
 
 void Player::setName()
 {
-    std::cout << "Enter the name of the player: ";
-    std::getline(std::cin, name);
-}
-
-int Player::getNumber()
-{
-    return playerNumber;
-}
-
-void Player::setNumber(int i)
-{
-    playerNumber = i;
+	std::cout << "Enter the name of the player: ";
+	std::getline(std::cin, name);
 }
 
 std::string Player::getName()
 {
-    return name;
+	return name;
+}
+
+void Player::setNumber(int i)
+{
+	playerNumber = i;
+}
+
+int Player::getNumber()
+{
+	return playerNumber;
 }
 
 void Player::addCardToHand(std::vector<std::string> card)
 {
-    hand.push_back(card);
+	hand.push_back(card);
 }
 
 std::vector<std::vector<std::string>> Player::getHand() {
-    return hand;
+	return hand;
 }
 
 void botPlayer::setBotName(const std::string& newName)
@@ -39,17 +39,101 @@ void botPlayer::setBotName(const std::string& newName)
 
 Action Player::chooseAction()
 {
-    std::cout << "Your move (Fold/Call/Raise/AllIn): ";
-    std::string action;
-    std::cin >> action;
-    if (action == "Fold") return Action::Fold;
-    else if (action == "Raise") return Action::Raise;
-	else if (action == "Call") return Action::Call;
-	else if (action == "AllIn") return Action::AllIn;
+	std::cout << "Your move (Fold/Bet/Raise/Call/AllIn): ";
+	std::string action;
+	std::cin >> action;
+	if (action == "Fold") {
+		return Action::Fold;
+	}
+	else if (action == "Bet") {
+		Player::bet();
+		return Action::Bet;
+	}
+	else if (action == "Raise") {
+		Player::raise();
+		return Action::Raise;
+	}
+	else if (action == "Call") {
+		Player::call();
+		return Action::Call;
+	}
+	else if (action == "AllIn") {
+		return Action::AllIn;
+	}
+}
+
+void Player::fold()
+{
+	folded = true;
+}
+
+void Player::bet() {
+	int betAmount = 0;
+	std::cout << "Enter the amount you want to bet: ";
+	std::cin >> betAmount;
+	if (betAmount > chips) {
+		std::cout << "You don't have enough chips to bet that amount!" << std::endl;
+		Player::bet(); // Ask for bet again
+		return;
+	}
+	chips -= betAmount;
+	currentBet = betAmount;
+	pot += betAmount;
+	std::cout << "You have bet " << betAmount << std::endl;
+}
+
+void Player::call()
+{
+	if (currentBet >= chips) {
+		pot += chips;
+		chips = 0;
+	}
+	else
+	{
+		chips -= currentBet;
+		pot += currentBet;
+	}
+}
+
+void Player::raise() {
+	if (currentBet >= chips) {
+		std::cout << "You don't have enough chips to raise!" << std::endl;
+		std::cout << "You can only fold or go all in." << std::endl;
+		std::string newAction;
+		std::cin >> newAction;
+		if (newAction == "fold") {
+			Player::fold();
+			return;
+		}
+		else if (newAction == "call" || newAction == "allin")
+		{
+			Player::call();
+			return;
+		}
+	}
+	else
+	{
+		int newAmount = 0;
+		std::cout << "I raise the bet to " << newAmount << std::endl;
+		std::cin >> newAmount;
+		if (newAmount > chips) {
+			std::cout << "You don't have enough chips to raise!" << std::endl;
+			Player::raise();
+			return;
+		}
+		else if (newAmount <= currentBet) {
+			std::cout << "You have to raise more than " << currentBet << "." << std::endl;
+			Player::raise();
+			return;
+		}
+		chips -= newAmount;
+		currentBet = newAmount;
+		pot += newAmount;
+	}
 }
 
 Action botPlayer::chooseAction()
 {
-    std::cout << "All In!" << std::endl;
+	std::cout << "All In!" << std::endl;
 	return Action::Call;
 }
--- Poker/player.h
+++ Poker/player.h
@@ -3,6 +3,7 @@
 #include <vector>  
 
 enum class Action {
+	Bet,
 	Fold,
 	Call,
 	Raise,
@@ -13,7 +14,10 @@ class Player
 {
 protected: // Change private members to protected to allow access in derived classes  
 	int playerNumber;
-	bool goodCombination = false;
+	int currentBet = 0;
+	int pot = 0;
+	int chips = 10000; // Each player starts with 10,000 chips
+	bool folded = false; // Flag to indicate if the player has folded
 	std::string name;
 	std::vector<std::vector<std::string>> hand; // Each player gets 2 cards  
 	std::string action;
@@ -27,11 +31,17 @@ public:
 	void addCardToHand(std::vector<std::string> card);
 	std::vector<std::vector<std::string>> getHand();
 	virtual Action chooseAction(); // virtual function to allow derived classes to implement their own action logic (override it))
+
+	void bet();
+	void call();
+	void fold();
+	void raise();
 };
 
 class botPlayer : public Player
 {
-	// Bot player class is used to implement AI for the game  
+	// Bot player class is used to implement AI for the game 
+	bool goodCombination = false;
 public:
 	void setBotName(const std::string& name);
 	Action chooseAction() override;
